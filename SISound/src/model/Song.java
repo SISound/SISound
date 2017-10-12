c3bca7c919649325860c060554a8533dd34cd30d
idoktorov <iliya.doktorov@gmail.com>
2017-10-12 23:16:05 +0300

    added Playlist and DBManager, some changes to Song

--- SISound/src/model/Song.java
+++ SISound/src/model/Song.java
@@ -3,18 +3,19 @@ package model;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.TreeSet;
 
-public class Song {
+public class Song implements Comparable<Song> {
 
-	private int songId;
+	private long songId;
 	private String title;
 	private LocalDateTime uploadDate;
 	private int timesListened;
 	private User user;
 	private String url;
 	private Genres genre;
-	private HashMap<Actions, ArrayList<User>> actions;
+	private HashMap<Actions, HashSet<User>> actions;
 	private TreeSet<Comment> comments;
 	
 	//constructor for adding song
@@ -25,16 +26,16 @@ public class Song {
 		this.url = url;
 		this.uploadDate = uploadDate;
 		this.timesListened = 0;
-		actions = new HashMap<>();
+		this.actions = new HashMap<>();
 		for (Actions action : Actions.values()) {
-			this.actions.put(action, new ArrayList<>());
+			this.actions.put(action, new HashSet<>());
 		}
-		comments = new TreeSet<>();
+		this.comments = new TreeSet<>();
 	}
 
 	//constructor for retrieving from db
-	public Song(int songId, String title, LocalDateTime uploadDate, int timesListened, User user, String url,
-			Genres genre, HashMap<Actions, ArrayList<User>> actions, TreeSet<Comment> comments) {
+	public Song(long songId, String title, LocalDateTime uploadDate, int timesListened, User user, String url,
+			Genres genre, HashMap<Actions, HashSet<User>> actions, TreeSet<Comment> comments) {
 		this(title, user, genre, url, uploadDate);
 		this.songId = songId;
 		this.timesListened = timesListened;
@@ -42,7 +43,7 @@ public class Song {
 		this.comments = comments;
 	}
 
-	public int getSongId() {
+	public long getSongId() {
 		return songId;
 	}
 
@@ -70,7 +71,7 @@ public class Song {
 		return genre;
 	}
 
-	public HashMap<Actions, ArrayList<User>> getActions() {
+	public HashMap<Actions, HashSet<User>> getActions() {
 		return actions;
 	}
 
@@ -92,5 +93,14 @@ public class Song {
 	
 	public void setGenre(Genres genre) {
 		this.genre = genre;
+	}
+	
+	public void addAction(Actions action, User user) {
+		this.actions.get(action).add(user);
+	}
+	
+	@Override
+	public int compareTo(Song o) {
+		return this.uploadDate.compareTo(o.getUploadDate());
 	}
 }
